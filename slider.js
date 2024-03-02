64a2e7aab38599b192a84694873f754679936fd8
Matheus Borges <matheusborges2045@outlook.com>
2024-03-01 21:08:44 -0300

    3

--- slider.js
+++ slider.js
@@ -1,10 +1,7 @@
 let slideIndex = 1;
         showSlides(slideIndex);
         
-        // Next/previous controls
-        function plusSlides(n) {
-            showSlides(slideIndex += n);
-        }
+
         
         // Thumbnail image controls
         function currentSlide(n) {
